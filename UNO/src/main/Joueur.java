fd8df013e3d8faafa4e8a0a1fd400c3f426ef9b1
Pierre Goujet <pierre.goujet.etu@univ-lille.fr>
2022-09-01 16:21:22 +0200

    Joueur quasi fini

--- UNO/src/main/Joueur.java
+++ UNO/src/main/Joueur.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Joueur {
 	private ArrayList<Carte> main;
+	private int ordre;
 
 	public ArrayList<Carte> getMain() {
 		return main;
@@ -16,8 +17,28 @@ public class Joueur {
 	public Joueur(ArrayList<Carte> main) {
 		super();
 		this.main = main;
+		this.ordre=0;
+	}
+
+	public int getOrdre() {
+		return ordre;
+	}
+
+	public void setOrdre(int ordre) {
+		this.ordre = ordre;
+	}
+	
+	public void addCarte(Carte c) {
+		this.main.add(c);
+	}
+	
+	public void removeCarte(int index) {
+		this.main.remove(index);
 	}
 	
 	
+	public void removeCarte(Carte c) {
+		this.main.remove(c);
+	}
+	
 }
-s
