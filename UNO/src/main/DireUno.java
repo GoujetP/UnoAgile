1bf5a2d284d37787973c542dc1c6941384f6d0ce
Pierre FOULON <pierre.foulon3.etu@hevea13.iut-infobio.priv.univ-lille1.fr>
2022-09-02 16:14:03 +0200

    Création du README et description des Sprints 1 à 4

--- UNO/src/main/DireUno.java
+++ UNO/src/main/DireUno.java
@@ -5,15 +5,15 @@ public class DireUno {
 	Joueur j;
 	
 	
-	public static boolean DireUno(Joueur j) {
+	public boolean DireUno(Joueur j) {
 		if(j.getNbCarte()==1) {
 			return true;
 		}
 		else {
 			return false;
-		}
-		
-		
+		}	
+	}
+	public static void main(String[] args) {
 		
 	}
 }
